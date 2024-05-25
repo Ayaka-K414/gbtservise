c30f93bed599b70a26d77a7edccc913294d50164
ayakakobayashi <414ayaka@gmail.com>
2024-05-25 23:00:20 +0900

    自動保存できるように設定しようとしてできていないまま質問中

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-export default {
-    root: 'src',
-    build: {
-      outDir: '../dist'
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: 'src',
+  build: {
+    outDir: '../dist'
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "src/style/scss/_base.scss" as *;
+          @use "src/style/scss/_header.scss" as *;`
+      }
+    }
+  },
+  server: {
+    watch: {
+      usePolling: true,
+      interval: 100,
     }
   }
-  
+});
